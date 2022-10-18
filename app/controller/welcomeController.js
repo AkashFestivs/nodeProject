5693dd3e28c06983887993ff907f48f3f7c775dc
Your Name <gadhaveakash999@gmail.com>
2022-10-18 18:51:26 +0530

    Without ajax Commit

--- app/controller/welcomeController.js
+++ app/controller/welcomeController.js
@@ -4,10 +4,8 @@ module.exports = {
     welcome: (req, res, next) =>{
         res.render('welcome');
     },
-    home: (req, res, next) =>{
-        console.log(req.query); 
-    },
     homepage:(req,res,next) =>{
+        console.log(req.query);
         res.render('home');
     }
 }
--- app/router/welcomeRouter.js
+++ app/router/welcomeRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const welcomeController = require('../controller/welcomeController');
 router.get('/welcome',welcomeController.welcome);
-router.get('/home', welcomeController.home);
 router.get('/homepage',welcomeController.homepage);
 module.exports = router;

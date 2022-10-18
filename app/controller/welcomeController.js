f4be478a160e246577b62d775b4fe468393ca9e9
Your Name <gadhaveakash999@gmail.com>
2022-10-18 18:44:48 +0530

    Changes

--- app/controller/welcomeController.js
+++ app/controller/welcomeController.js
@@ -1,10 +1,13 @@
+
+
 module.exports = {
     welcome: (req, res, next) =>{
         res.render('welcome');
     },
     home: (req, res, next) =>{
-        const bodyContent = req.body;
-        console.log('Data : ',bodyContent)
-        res.render('home.ejs');
-        }
+        console.log(req.query); 
+    },
+    homepage:(req,res,next) =>{
+        res.render('home');
+    }
 }
